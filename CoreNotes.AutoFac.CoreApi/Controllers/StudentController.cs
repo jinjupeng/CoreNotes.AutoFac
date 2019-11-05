3b6b57cecb0751a200a7ee3d47c9844add6811bf
jinjupeng <im.jp@outlook.com>
2019-11-05 13:15:23 +0800

    格式化返回数据格式

--- CoreNotes.AutoFac.CoreApi/Controllers/StudentController.cs
+++ CoreNotes.AutoFac.CoreApi/Controllers/StudentController.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using CoreNotes.AutoFac.IService;
+﻿using CoreNotes.AutoFac.IService;
+using CoreNotes.AutoFac.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreNotes.AutoFac.CoreApi.Controllers
@@ -7,8 +7,7 @@ namespace CoreNotes.AutoFac.CoreApi.Controllers
     /// <summary>
     /// 学生模块接口
     /// </summary>
-    [ApiController]
-    [Route("[controller]")]
+    [Route("[controller]/[action]")]
     public class StudentController : ControllerBase
     {
         private readonly IStudentService _studentService;
@@ -18,11 +17,17 @@ namespace CoreNotes.AutoFac.CoreApi.Controllers
         }
 
         [HttpGet]
-        public ActionResult GetStuNameById(/* string id */)
+        public ActionResult GetName(/* string id */)
         {
             string id = "236d37d4f3c14dd489b1c7270c7670ad";
-            throw new Exception("出错了");
-			return Ok(_studentService.GetStuName(id));
+            // throw new Exception("出错了");
+            MessageModel<string> message = new MessageModel<string>
+            {
+                Msg = "获取成功！",
+                Success = true,
+                Response = _studentService.GetStuName(id)
+            };
+            return Ok(message);
         }
     }
 }
---
+++ CoreNotes.AutoFac.Model/MessageModel.cs
@@ -0,0 +1,22 @@
+﻿namespace CoreNotes.AutoFac.Model
+{
+    /// <summary>
+    /// 通用返回信息类
+    /// </summary>
+    public class MessageModel<T>
+    {
+        /// <summary>
+        /// 操作是否成功
+        /// </summary>
+        public bool Success { get; set; } = false;
+        /// <summary>
+        /// 返回信息
+        /// </summary>
+        public string Msg { get; set; } = "服务器异常";
+        /// <summary>
+        /// 返回数据集合
+        /// </summary>
+        public T Response { get; set; }
+
+    }
+}
